e8b278cd39a71222d106ce21506f8b6e452772bc
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2013-11-01 12:36:39 +0100

    Entry preconditions

--- src/main/java/pl/rawie/timetrack/domain/model/Entry.java
+++ src/main/java/pl/rawie/timetrack/domain/model/Entry.java
@@ -3,9 +3,9 @@ package pl.rawie.timetrack.domain.model;
 import com.google.common.collect.Range;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
-import org.joda.time.Interval;
 
-import java.util.Date;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Entry {
     private String summary;
@@ -13,6 +13,14 @@ public class Entry {
     private DateTime end;
 
     public Entry(String summary, DateTime start, DateTime end) {
+        checkNotNull(summary, "summary");
+        checkArgument(!summary.isEmpty(), "summary");
+        checkNotNull(start, "start");
+        checkNotNull(end, "end");
+        checkArgument(start.isBefore(end), "start must be before end");
+        checkArgument(start.withTimeAtStartOfDay().isEqual(end.withTimeAtStartOfDay()),
+                "start and end must be at the same day");
+
         this.summary = summary;
         this.start = start;
         this.end = end;
---
+++ src/test/java/pl/rawie/timetrack/domain/model/EntryTest.java
@@ -0,0 +1,62 @@
+package pl.rawie.timetrack.domain.model;
+
+import org.junit.Test;
+import pl.rawie.timetrack.utils.Today;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static pl.rawie.timetrack.domain.model.SampleEntry.builder;
+
+public class EntryTest {
+    private void expectNullPointerException(EntryBuilder builder, String message) {
+        try {
+            builder.build();
+            fail("Should failed");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage(), is(equalTo(message)));
+        }
+    }
+
+    private void expectIllegalArgumentException(EntryBuilder builder, String message) {
+        try {
+            builder.build();
+            fail("Should failed");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is(equalTo(message)));
+        }
+    }
+
+    @Test
+    public void nullSummary() {
+        expectNullPointerException(builder().withSummary(null), "summary");
+    }
+
+    @Test
+    public void emptySummary() {
+        expectIllegalArgumentException(builder().withSummary(""), "summary");
+    }
+
+    @Test
+    public void nullStart() {
+        expectNullPointerException(builder().withStart(null), "start");
+    }
+
+    @Test
+    public void nullEnd() {
+        expectNullPointerException(builder().withEnd(null), "end");
+    }
+
+    @Test
+    public void startAfterEnd() {
+        expectIllegalArgumentException(builder().withStart(Today.withTime(14)).withEnd(Today.withTime(12)),
+                "start must be before end");
+    }
+
+    @Test
+    public void startFromDifferentDayThanEnd() {
+        expectIllegalArgumentException(builder().withStart(Today.withTime(12).minusDays(1)),
+                "start and end must be at the same day");
+    }
+}
