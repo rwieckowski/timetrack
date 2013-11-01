8520bf3cc6e4643d56e436fd2346fa897bf5e95d
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2013-11-01 14:16:20 +0100

    Week property

--- src/main/java/pl/rawie/timetrack/domain/model/WeeklyEntries.java
+++ src/main/java/pl/rawie/timetrack/domain/model/WeeklyEntries.java
@@ -1,12 +1,21 @@
 package pl.rawie.timetrack.domain.model;
 
+import com.google.common.collect.Range;
+import org.joda.time.DateTime;
+
 import java.util.List;
 
 public class WeeklyEntries {
+    private Range<DateTime> week;
     private List<Entry> entries;
 
-    public WeeklyEntries(List<Entry> entries) {
+    public WeeklyEntries(Range<DateTime> week, List<Entry> entries) {
+        this.week = week;
         this.entries = entries;
+    }
+
+    public Range<DateTime> getWeek() {
+        return week;
     }
 
     public List<Entry> getEntries() {
